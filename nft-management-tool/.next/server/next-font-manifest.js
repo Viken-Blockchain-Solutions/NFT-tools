a95431b22b22744ccdd911c404836a5878de87f3
ivo garofalo <41997352+Dadogg80@users.noreply.github.com>
2023-05-13 18:09:35 +0200

    w

---
+++ nft-management-tool/.next/server/next-font-manifest.js
@@ -0,0 +1,10 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {
+    "/Users/ivo_centaurify/Repository/viken_blockchain/NFT-tools/nft-management-tool/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Montserrat\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"mont\"}": [
+      "static/media/1060bab20f18b5c2-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
