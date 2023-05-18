f50dc5bab58e548b76009f48ee8d930ff9d7db77
ivo garofalo <ivo.garofalo@gmail.com>
2023-05-18 14:26:30 +0200

    add: tailwind elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/tw-elements/dist/js/**/*.js",
+
   ],
   theme: {
     extend: {
@@ -14,5 +16,6 @@ module.exports = {
       },
     },
   },
+  darkMode: 'class',
   plugins: [require('tw-elements/dist/plugin')],
 }
