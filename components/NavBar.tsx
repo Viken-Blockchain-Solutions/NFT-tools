e9b359a653bb158067bfa1896c761202827020bb
ivo garofalo <ivo.garofalo@gmail.com>
2023-05-18 14:59:48 +0200

    add: logo to navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,8 @@
+import Image from 'next/image'
+import logo from '../public/Viken.jpg'
+
 const NavBar = () => {
   
-
   return (
      <>
       <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
@@ -31,9 +33,7 @@ const NavBar = () => {
               </svg>
             </button>
             <a className="navbar-brand text-blue-600" href="#!">
-              <svg className="w-5 h-5 ml-2 lg:ml-0 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                <path fill="currentColor" d="M485.5 0L576 160H474.9L405.7 0h79.8zm-128 0l69.2 160H149.3L218.5 0h139zm-267 0h79.8l-69.2 160H0L90.5 0zM0 192h100.7l123 251.7c1.5 3.1-2.7 5.9-5 3.3L0 192zm148.2 0h279.6l-137 318.2c-1 2.4-4.5 2.4-5.5 0L148.2 192zm204.1 251.7l123-251.7H576L357.3 446.9c-2.3 2.7-6.5-.1-5-3.2z"></path>
-              </svg>
+              <Image src={logo} alt='vbs'width={100}  height={100}/>
             </a>
           </div>
           <div className="navbar-collapse collapse grow items-center" id="navbarSupportedContentY">
