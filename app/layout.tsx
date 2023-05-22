db611b1510788f49f812a02fd1009b9067ce0710
ivo garofalo <41997352+Dadogg80@users.noreply.github.com>
2023-05-22 17:42:28 +0200

    add: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from '@components/Footer';
 import NavBar from '@components/NavBar';
+import { Analytics } from '@vercel/analytics/react';
 import 'styles/globals.css';
 import "tw-elements/dist/css/tw-elements.min.css";
 import { Roboto } from "next/font/google";
@@ -7,8 +8,8 @@ import { Roboto } from "next/font/google";
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export const metadata = {
-  title: 'NFT Collection Management Tool',
-  description: 'A tool for managing NFT collections',
+  title: 'NFTInsight',
+  description: 'NFTInsight is an analytics and management tool for NFT Creators and Deployers',
 }
 
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
