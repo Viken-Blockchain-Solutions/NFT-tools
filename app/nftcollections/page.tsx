aab8dd6934d62210195a31dfa0002f07dd93679f
ivo garofalo <41997352+Dadogg80@users.noreply.github.com>
2023-06-07 02:34:38 +0200

    add: db

--- app/nftcollections/page.tsx
+++ app/nftcollections/page.tsx
@@ -6,13 +6,27 @@ import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 
 
+export type UserResponse = [{
+    email: string;
+    image: string;
+    nftcollection: [
+        {
+            collectionAddress: string;
+            id: number;
+        }
+    ];
+    __v: number;
+    username: string;
+    _id: string;
+}]
+
 
 const NFTCollectionPage = () => {
     const { data: session } = useSession();
     const router = useRouter();
     const [collectionAddress, setCollectionAddress] = useState<any>({});
     const [collectionsList, setCollectionsList] = useState<any>([]);
-    const [dbData, setDBData] = useState<[]>()
+    const [dbData, setDBData] = useState<UserResponse>();
     const [submitting, setSubmitting] = useState<boolean>(false);
 
     
@@ -21,34 +35,22 @@ const NFTCollectionPage = () => {
             router.push("/");
         }
         const fetchFromDB = async () => {
-            console.log("Fetching From DB...")
+
             try {
                 const response = await fetch("/api/nftcollections",);
-                console.log("This is response:", response)
-                const data = await response.json()
-                console.log("Data form DB: ", data);
-                setDBData(data);
+                
+                if (!response.ok) {
+                    throw new Error("Not ok response")
+                }
+
+                const data: UserResponse = await response.json()
+                setDBData(data)
+
             } catch (error) {
                 console.log("Error fetching from DB: ", error)
             }
-
-
-            /* dbData.map((nftcollection: any) => {
-                nftcollection.contractAddress.map((address: string, index: number) => {
-                    setCollectionsList((prevState: any) => ({
-                        ...prevState,
-                        [address]: {
-                            address: address,
-                            id: index,
-                        },
-                    }));
-                });
-            }); */
         };
         fetchFromDB();
-        console.log("DBData: ", dbData);
-        console.log("collectionsList: ", collectionsList);
-
     }, []);
 
 
@@ -65,21 +67,23 @@ const NFTCollectionPage = () => {
 
 
                 <hr className="my-8 bg-purple-700 w-1/2" />
-                <TabWindow />
+                <TabWindow dbData={dbData} />
+                
+                {/* 
                 <hr className="my-8 bg-purple-700 w-1/2" />
-                {/* <AddressForm
-                    setCollectionAddress={setCollectionAddress}
-                    submitting={submitting}
-                /> 
+                    <AddressForm
+                        setCollectionAddress={setCollectionAddress}
+                        submitting={submitting}
+                    /> 
 
-                <div>
-                    {collectionsList && Object.keys(collectionsList).map((key: any) => (
-                        <div key={key}>
-                            <div>{collectionsList[key].address}</div>
-                            <div>{collectionsList[key].id}</div>
-                        </div>
-                    ))}
-                </div>
+                    <div>
+                        {collectionsList && Object.keys(collectionsList).map((key: any) => (
+                            <div key={key}>
+                                <div>{collectionsList[key].address}</div>
+                                <div>{collectionsList[key].id}</div>
+                            </div>
+                        ))}
+                    </div>
                 */}
             </div>
         </>
--- app/nftcollections/sections/tabwindow.tsx
+++ app/nftcollections/sections/tabwindow.tsx
@@ -1,9 +1,10 @@
 'use client'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Stats from './stats';
 
 
-const SelectedTab = ({ activeTab }: { activeTab: number }) => {
+
+const SelectedTab = ({activeTab}: {activeTab: number}) => {
     
     return (
         <div className={activeTab === 1 ? "content content-active" : "content"}>
@@ -12,36 +13,64 @@ const SelectedTab = ({ activeTab }: { activeTab: number }) => {
     )
 }
 
-function TabWindow() {
+function TabWindow(dbData: any) {
     const [activeTab, setActiveTab] = useState<number>(1);
+    const [collectionData, setCollectionData] = useState([]);
+
+    useEffect(() => {
+        const data = dbData?.dbData;
+        const getAddresses = async () => {
+            const col: any = [];
+
+            if (data) {
+                data.map((user: any) => {
+                    console.log(user)
+                    user.nftCollections.map((nft: any) => {
+                        col.push(nft.nftcollection)
+                    })
+                })
+            }
+            console.log("Collections: ", col)
+            setCollectionData(col)
+        }
+        getAddresses();
+
+    }, [dbData?.dbData]);
 
     const toggleTab = (tab: number) => {
         setActiveTab(tab);
+        console.log("hey")
     }
 
     return (
         <>
-            <div className="tabs">
-                <a 
-                    className={activeTab === 1 ? "tab tab-lg tab-lifted tab-active" : "tab tab-lg"}
-                    onClick={() => toggleTab(1)}
-                >
-                    Collection 1
-                </a> 
-                <a 
-                    className={activeTab === 2 ? "tab tab-lg tab-lifted tab-active" : "tab tab-lg"}
-                    onClick={() => toggleTab(2)}
-                >
-                    Collection 2
-                </a> 
-                <a 
-                    className={activeTab === 3 ? "tab tab-lg tab-lifted tab-active" : "tab tab-lg"}
-                    onClick={() => toggleTab(3)}
-                >
-                    Collection 3
-                </a>
-            </div>
-            <SelectedTab activeTab={activeTab} />
+        {collectionData?.length > 0 && (
+            <section>
+                <div className="tabs">
+                    <a 
+                        className={activeTab === 1 ? "tab tab-lg tab-lifted tab-active" : "tab tab-lg"}
+                        onClick={() => toggleTab(1)}
+                        >
+                        Collection {collectionData?.length}
+                    </a> 
+                    <a 
+                        className={activeTab === 2 ? "tab tab-lg tab-lifted tab-active" : "tab tab-lg"}
+                        onClick={() => toggleTab(2)}
+                        >
+                        Collection 2
+                    </a> 
+                    <a 
+                        className={activeTab === 3 ? "tab tab-lg tab-lifted tab-active" : "tab tab-lg"}
+                        onClick={() => toggleTab(3)}
+                        >
+                        Collection 3
+                    </a>
+                </div>
+                {/* <SelectedTab 
+                activeTab={activeTab}
+                /> */}
+            </section>
+            )}
         </>
     );
 }
