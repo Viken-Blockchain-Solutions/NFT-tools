824c549e8246e3d505a896ea2880c79eb335f3cc
ivo garofalo <41997352+Dadogg80@users.noreply.github.com>
2023-06-11 18:09:08 +0200

    add: GET and EDIT

---
+++ app/api/nftcollections/[id]/route.ts
@@ -0,0 +1,55 @@
+import { IUser } from '@/models/user';
+import { connectToDB } from '@lib/database';
+import User from '@models/user';
+import NFTCollection from '@/models/nftCollection';
+
+// GET
+export const GET = async ( request: any, { params }: any  ) => {
+
+    try {
+        await connectToDB();
+        // find all collections by deployer
+        const collection = await NFTCollection.findById(params.id);
+        if(!collection) {
+            return new Response(JSON.stringify("Collection not found"), {status: 404});
+        }
+    
+        return new Response(JSON.stringify(collection), {status: 200});
+        
+    } catch (error) {
+        console.log("error:", error);
+        return new Response(JSON.stringify("Failed to fetch all collections from DB"), 
+            {status: 500});
+        
+    }
+
+}
+
+// PATCH
+export const PATCH = async ( request: any, { params }: any  ) => {
+    const { name, description, image, deployer } = await request.json();
+
+    try {
+        await connectToDB();
+        const existingCollection = await NFTCollection.findById(params.id);
+        if(!existingCollection) {
+            return new Response(JSON.stringify("Collection not found"), {status: 404});
+        }
+
+        existingCollection.name = name;
+        existingCollection.description = description;
+        existingCollection.image = image;
+        existingCollection.deployer = deployer;
+
+        await existingCollection.save();
+
+        return new Response(JSON.stringify(existingCollection), {status: 200});
+    }
+    catch (error) {
+        console.log("error:", error);
+        return new Response(JSON.stringify("Failed to fetch all collections from DB"),
+
+            {status: 500});
+    }
+}
+// DELETE
